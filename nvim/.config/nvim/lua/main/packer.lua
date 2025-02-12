ac139812d4ec9ee71031b3083d16abde54544142
Aaryan <47253380+aaryanDhakal22@users.noreply.github.com>
2025-02-12 00:16:17 -0500

    Added ability to bootstrap it

--- nvim/.config/nvim/lua/main/packer.lua
+++ nvim/.config/nvim/lua/main/packer.lua
@@ -1,7 +1,18 @@
-vim.cmd.packadd('packer.nvim')
+local ensure_packer = function()
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+end
+return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  use ('wbthomason/packer.nvim')
+  use 'wbthomason/packer.nvim'
   use 'm4xshen/autoclose.nvim'
   use {
   'nvim-telescope/telescope.nvim', tag = '0.1.8',
@@ -38,5 +49,7 @@ return require('packer').startup(function(use)
 
   use("folke/zen-mode.nvim")
   use("eandrju/cellular-automaton.nvim")
-
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
