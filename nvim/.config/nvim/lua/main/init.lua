8b3224cd20a44c438510ca5b91971efefe77c087
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2025-02-12 20:03:35 -0500

    Changed packer to lazy vim

--- nvim/.config/nvim/lua/main/init.lua
+++ nvim/.config/nvim/lua/main/init.lua
@@ -1,18 +1,19 @@
 require("main.remap")
 require("main.set")
-require("autoclose").setup()
-require('mason').setup()
-require("mason-lspconfig").setup_handlers {
-    -- The first entry (without a key) will be the default handler
-    -- and will be called for each installed server that doesn't have
-    -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
-    end,
-    -- Next, you can provide a dedicated handler for specific servers.
-    -- For example, a handler override for the `rust_analyzer`:
-    ["rust_analyzer"] = function ()
-        require("rust-tools").setup {}
-    end
-}
+require("main.lazy")
+-- require("autoclose").setup()
+-- require('mason').setup()
+-- require("mason-lspconfig").setup_handlers {
+--     -- The first entry (without a key) will be the default handler
+--     -- and will be called for each installed server that doesn't have
+--     -- a dedicated handler.
+--     function (server_name) -- default handler (optional)
+--         require("lspconfig")[server_name].setup {}
+--     end,
+--     -- Next, you can provide a dedicated handler for specific servers.
+--     -- For example, a handler override for the `rust_analyzer`:
+--     ["rust_analyzer"] = function ()
+--         require("rust-tools").setup {}
+--     end
+-- }
 
--- nvim/.config/nvim/lua/main/set.lua
+++ nvim/.config/nvim/lua/main/set.lua
@@ -1,7 +1,7 @@
 vim.opt.guicursor = ""
-vim.opt.nu = true
+vim.opt.number = true
 vim.opt.relativenumber = true
-
+vim.opt.autoindent = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
