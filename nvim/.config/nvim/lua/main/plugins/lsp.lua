0c4b49f7a5b60e4cf53db3828ef966a9a37547a3
aaryanDhakal22 <aaryan.dhakal22@gmail.com>
2025-02-12 21:54:54 -0500

    added some dependencies

--- nvim/.config/nvim/lua/main/plugins/lsp.lua
+++ nvim/.config/nvim/lua/main/plugins/lsp.lua
@@ -115,7 +115,8 @@ return { -- LSP Configuration & Plugins
     'williamboman/mason.nvim',
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/nvim-cmp',
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
     {
